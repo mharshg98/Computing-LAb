180f5368246960cb813fbd59710b8ddf6cff9b12
abhinaba <abhinaba.fbr@gmail.com>
2022-01-03 01:27:52 +0530

    slider fixed, images fixed, announcement button added

--- static/js/admin/script.js
+++ static/js/admin/script.js
@@ -1,13 +1,15 @@
 let slider=document.getElementById("slider");
-let btn1=document.getElementById("btn1");
-let btn2=document.getElementById("btn2");
+let indicators=document.getElementById("indicators");
+//let btn1=document.getElementById("btn1");
+//let btn2=document.getElementById("btn2");
 
-btn1.addEventListener("click", function(e){
-    console.log("btn1 clicked");
-    slider.style.transform="translateX(0px)";
-})
+let l=slider.childElementCount;
+slider.style.width=(l*100)+"%";
 
-btn2.addEventListener("click", function(e){
-    console.log("btn2 clicked");
-    slider.style.transform="translateX(-100%)";
-})
+for (let i=0;i<indicators.childElementCount;i++){
+    indicators.children[i].addEventListener("click", function(e){
+        console.log("btn1 clicked");
+        let shift=(i/l)*100;
+        slider.style.transform="translateX(-"+shift+"%)";
+    });
+}
